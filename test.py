1271a078f1bd6058fba0f39935e11a29f9842d6d
huangqiangsheng <huangqiangsheng@wanda.cn>
2018-09-21 19:10:18 +0800

    developping

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ import unittest
 class CymongoTest(unittest.TestCase):
     mongo_uri = 'mongodb://hqs:f@localhost:27017/test'
     db = 'test'
-    enable_debug = True
+    enable_debug = False
     index_name = 'date_time'
     column_name = 'blogger_id'
     value_names = [  # 'signature', 'last_logged_date_time'
